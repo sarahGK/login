01150175b0fc425aea5567b13f41a035914fc03f
sarahGK <xin.kelly@hotmail.com>
2017-03-09 10:05:23 -0800

    Update testtwiolio.py
    Try use Twilio to validate the phone number exists or not

--- login/testtwiolio.py
+++ login/testtwiolio.py
@@ -3,13 +3,13 @@ from twilio.rest.exceptions import TwilioRestException
 
 client = TwilioRestClient(account='AC8cdba7c0b7a032d1bc17e13f02bd1b31',
                           token='4d60f1e68a313b9ce43866cb40253d3b')
-#try:
-response = client.caller_ids.get('+16505188115')
-#except TwilioRestException as e:
- # if e.code == 20404:
- #   print("The phone number does not exist.")
- # else:
-    #raise e
+try:
+response = client.caller_ids.get('+1234567890')
+except TwilioRestException as e:
+  if e.code == 20404:
+    print("The phone number does not exist.")
+  else:
+    raise e
 
 print(response.validation_code)
 
