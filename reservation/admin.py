0fe4841b167b6ce46c79b20dfd9353b8a4b4ca6c
sarahGK <xin.kelly@hotmail.com>
2017-03-09 10:13:43 -0800

    Update admin.py

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from login.models import Profile
 from .models import Spot,Reservation
-
+# use 'python manage.py createsuperuser' to create superuser then can use /hostname/admin to look up all the registered models
+'''
+Username: xin
+Email address: xin.kelly@hotmail.com
+password:123456
+'''
 # Register your models here.
 
 admin.site.register(Profile)
